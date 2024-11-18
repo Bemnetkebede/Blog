59b87be1d89805acc8e8849cff69ef8f179379cb
Bemnet Kebede <bemnet026@gmail.com>
2024-11-18 13:46:15 +0300

    Fix: Corrected port usage for Render deployment

--- InterTechHub/server.js
+++ InterTechHub/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes/index')
 
 app.use('/', routes)
 
-const port = 5000
+const port = process.env.PORT ||  5000
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`)
 })
